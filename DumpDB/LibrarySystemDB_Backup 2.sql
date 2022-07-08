031687aeefdcfab021b0906d45162729a69b8641
Tharindu Romesh <91407033+tharinduromesh@users.noreply.github.com>
2022-07-08 14:11:38 +0530

    sql dump

--- DumpDB/LibrarySystemDB_Backup 2.sql
+++ DumpDB/LibrarySystemDB_Backup 2.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
--- Host: localhost    Database: LibrarySystemDB
+-- Host: localhost    Database: librarydb
 -- ------------------------------------------------------
--- Server version	8.0.29-0ubuntu0.22.04.2
+-- Server version	8.0.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,39 +16,40 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Book`
+-- Table structure for table `book`
 --
 
-DROP TABLE IF EXISTS `Book`;
+DROP TABLE IF EXISTS `book`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Book` (
+CREATE TABLE `book` (
   `BookID` int NOT NULL AUTO_INCREMENT,
   `BookName` varchar(45) NOT NULL,
   `BookAuthor` varchar(45) NOT NULL,
   `BookCategory` varchar(45) NOT NULL,
   PRIMARY KEY (`BookID`),
   UNIQUE KEY `BookID_UNIQUE` (`BookID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Book`
+-- Dumping data for table `book`
 --
 
-LOCK TABLES `Book` WRITE;
-/*!40000 ALTER TABLE `Book` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Book` ENABLE KEYS */;
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'Madol Duwa','Martin','Novel');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Staff`
+-- Table structure for table `staff`
 --
 
-DROP TABLE IF EXISTS `Staff`;
+DROP TABLE IF EXISTS `staff`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Staff` (
+CREATE TABLE `staff` (
   `StaffID` int NOT NULL AUTO_INCREMENT,
   `UserName` varchar(45) NOT NULL,
   `Password` varchar(45) NOT NULL,
@@ -59,23 +60,23 @@ CREATE TABLE `Staff` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Staff`
+-- Dumping data for table `staff`
 --
 
-LOCK TABLES `Staff` WRITE;
-/*!40000 ALTER TABLE `Staff` DISABLE KEYS */;
-INSERT INTO `Staff` VALUES (1,'Kavindra','12345');
-/*!40000 ALTER TABLE `Staff` ENABLE KEYS */;
+LOCK TABLES `staff` WRITE;
+/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+INSERT INTO `staff` VALUES (1,'Kavindra','12345');
+/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `new_table`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `new_table`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `new_table` (
+CREATE TABLE `user` (
   `UserID` int NOT NULL AUTO_INCREMENT,
   `UserName` varchar(45) NOT NULL,
   `DOB` date NOT NULL,
@@ -93,12 +94,12 @@ CREATE TABLE `new_table` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `new_table`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `new_table` WRITE;
-/*!40000 ALTER TABLE `new_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `new_table` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -110,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-08 13:55:58
+-- Dump completed on 2022-07-08 14:10:34

73b352929d47fb945b775c87f51b796b41dec5b5
Tharindu Romesh <91407033+tharinduromesh@users.noreply.github.com>
2022-07-08 14:53:28 +0530

    search box query

--- src/main/java/library/librarysystem/Controller/SearchBookController.java
+++ src/main/java/library/librarysystem/Controller/SearchBookController.java
@@ -2,11 +2,20 @@ package library.librarysystem.Controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+import library.librarysystem.DBConnection.DBHandler;
 
-public class SearchBookController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Handler;
+
+public class SearchBookController implements Initializable {
 
     @FXML
     private TextField authorInput;
@@ -23,16 +32,47 @@ public class SearchBookController {
     @FXML
     private TextArea outPutTextArea;
 
+
+    private DBHandler handler;
+    private Connection connection;
+    private PreparedStatement pst;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        handler= new DBHandler();
+    }
+
+
     @FXML
     void searchBookFromAuthor(ActionEvent event) {
+
 
     }
 
     @FXML
     void searchBookfromBookName(ActionEvent event) {
         String name = bookNameInput.getText();
+
+        connection = handler.getConnection();
+
+        String getDetailsQuery = "SELECT * FROM book WHERE BookName LIKE"+"\'"+"%"+"?"+"\'";
+
+        try {
+            pst = connection.prepareStatement(getDetailsQuery);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            pst.setString(1,name);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         outPutTextArea.setText("Hello WDK : " + name);
 
     }
+
 
 }

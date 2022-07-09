8234eb7d60b3ab432c5c62a7cfb86e63805bce18
Kavindra Weerasinghe <wdilshankavindra@gmail.com>
2022-07-09 14:17:13 +0530

    Admin Login completed

--- src/main/java/library/librarysystem/Controller/AdminLoginController.java
+++ src/main/java/library/librarysystem/Controller/AdminLoginController.java
@@ -3,8 +3,9 @@ package library.librarysystem.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import library.librarysystem.DBConnection.DBHandler;
 
 import java.net.URL;
@@ -17,10 +18,11 @@ import java.util.ResourceBundle;
 public class AdminLoginController implements Initializable {
 
     @FXML
-    private Button AddLoggingButton;
+    private Button loginButton;
 
     @FXML
-    private TextField UserInputPassword;
+    private PasswordField userInputPassword;
+
 
     private DBHandler handler;
     private Connection connection;
@@ -37,13 +39,13 @@ public class AdminLoginController implements Initializable {
 
 
     @FXML
-    void userLogin(ActionEvent event) {
+    void adminLogin(ActionEvent event) {
         String password;
-        password = UserInputPassword.getText();
+        password = userInputPassword.getText();
 
         connection = handler.getConnection();
 
-        String adminQuery = "SELECT * FROM Staff WHERE  UserName = ?";
+        String adminQuery = "SELECT * FROM staff WHERE  UserName = ?";
 
         try {
             pst = connection.prepareStatement(adminQuery);
@@ -58,27 +60,30 @@ public class AdminLoginController implements Initializable {
             ResultSet result = pst.executeQuery();
 
             boolean found = false;
-
+            String userPasswordFromDB="";
             while (result.next()) {
 
-                String userPasswordFromDB = result.getString("Password");
+                userPasswordFromDB = result.getString("Password");
             }
-            if (password == "Password") {
+            if (password.equals(userPasswordFromDB)) {
                 found = true;
-            System.out.println("Loggin Successfull");
 
-        }else{
-            System.out.println("System Logging Failed");
-        }
+                System.out.println("Loggin Successfull");
+
+            }else{
+                System.out.println("Incorrect Password");
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setContentText("Incorrect Password");
+                alert.show();
+
+            }
 
 
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-
     }
-
-
 }

c87a0befeba93ba0849deb1c1417e576fd60237c
isharasuvini <108082895+isharasuvini@users.noreply.github.com>
2022-07-09 13:54:18 +0530

    Update AdminLoginController.java

--- src/main/java/library/librarysystem/Controller/AdminLoginController.java
+++ src/main/java/library/librarysystem/Controller/AdminLoginController.java
@@ -1,4 +1,84 @@
 package library.librarysystem.Controller;
 
-public class AdminLoginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import library.librarysystem.DBConnection.DBHandler;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class AdminLoginController implements Initializable {
+
+    @FXML
+    private Button AddLoggingButton;
+
+    @FXML
+    private TextField UserInputPassword;
+
+    private DBHandler handler;
+    private Connection connection;
+    private PreparedStatement pst;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        handler = new DBHandler();
+
+    }
+
+
+
+
+    @FXML
+    void userLogin(ActionEvent event) {
+        String password;
+        password = UserInputPassword.getText();
+
+        connection = handler.getConnection();
+
+        String adminQuery = "SELECT * FROM Staff WHERE  UserName = ?";
+
+        try {
+            pst = connection.prepareStatement(adminQuery);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        try {
+            pst.setString(1, "admin");
+
+            ResultSet result = pst.executeQuery();
+
+            boolean found = false;
+
+            while (result.next()) {
+
+                String userPasswordFromDB = result.getString("Password");
+            }
+            if (password == "Password") {
+                found = true;
+            System.out.println("Loggin Successfull");
+
+        }else{
+            System.out.println("System Logging Failed");
+        }
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
 }

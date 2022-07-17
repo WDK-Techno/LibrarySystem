6efce1501e989a1176bcae0c3eb6ae8aeff597b9
Kavindra Weerasinghe <wdilshankavindra@gmail.com>
2022-07-18 04:09:56 +0530

    UI Connect Update

--- src/main/java/library/librarysystem/ControllerMainUI/MainUIController.java
+++ src/main/java/library/librarysystem/ControllerMainUI/MainUIController.java
@@ -14,6 +14,7 @@ import library.librarysystem.LibraryApp;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 
 public class MainUIController implements Initializable {
 
@@ -31,7 +32,6 @@ public class MainUIController implements Initializable {
     }
 
 
-
     private void setNode (Node node){
         loadpagepane.getChildren().clear();
         loadpagepane.getChildren().add((Node) node);
@@ -47,13 +47,42 @@ public class MainUIController implements Initializable {
 
     private void loadChildPage(){
         try {
-
-
            home = FXMLLoader.load(LibraryApp.class.getResource("MainUI_Home.fxml"));
-
             setNode(home);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+    @FXML
+    void loadAdmin(ActionEvent event) {
+        try {
+            home = FXMLLoader.load(LibraryApp.class.getResource("AdminLogin.fxml"));
+            setNode(home);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void loadHome(ActionEvent event) {
+        try {
+            home = FXMLLoader.load(LibraryApp.class.getResource("MainUI_Home.fxml"));
+            setNode(home);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    void loadManage(ActionEvent event) {
+        try {
+            home = FXMLLoader.load(LibraryApp.class.getResource("MainUI_Manage.fxml"));
+            setNode(home);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }

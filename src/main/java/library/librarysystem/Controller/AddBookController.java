0c95f8a9bd273629192de379fdde7ae73ebdcf92
Kavindra Weerasinghe <wdilshankavindra@gmail.com>
2022-09-06 12:01:12 +0530

    re

--- src/main/java/library/librarysystem/Controller/AddBookController.java
+++ src/main/java/library/librarysystem/Controller/AddBookController.java
@@ -121,11 +121,10 @@ public class AddBookController implements Initializable {
                     System.out.println("Book name :" +bookNameFromDb);
                     System.out.println("Book ID :"+ bookIdFromDb);
 
-                    BookID.setText( bookNameFromDb+"\t Book ID : "+bookIdFromDb);
+                    BookID.setText("Book ID : "+bookIdFromDb);
 
                     //clear fields
                     Author.setText("");
-                    BookID.setText("");
                     BookName.setText("");
                     Category.setText("");
 

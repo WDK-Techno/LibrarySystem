426c5da0bfa1a32b845f71ef49159ceba2204944
Kavindra Weerasinghe <wdilshankavindra@gmail.com>
2022-09-06 14:57:02 +0530

    Update DBHandler.java

--- src/main/java/library/librarysystem/DBConnection/DBHandler.java
+++ src/main/java/library/librarysystem/DBConnection/DBHandler.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBHandler {
+public class DBHandler extends Configs {
 
     Connection dbconnection;
 
